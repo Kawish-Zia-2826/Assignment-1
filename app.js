f0926819cf1564346eaafd484f084cd816942f57
Kawish Zia <kawishzia2826@gmail.com>
2024-11-01 02:52:45 -0700

    assignment 1.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// //-----------------------------------------------------Assignment01(Alert)---------------------------------------------------------------------------
+
+// // -------------------------------------------------------------TAsk No 1-----------------------------------------------------------------------------
+// // Write a script to greet your website visitor using JS alert
+// // box.
+// // 2. Write a script to display following message on your web
+
+//  alert("please Enter a Valid password");
+
+// // -------------------------------------------------------------TAsk No 2-----------------------------------------------------------------------------
+
+// // Write a script to display following message on your web
+// // page: (Hint : Use line break)
+//  alert("Wlcome to JS Land \n HappyCoding!");
+
+// // -------------------------------------------------------------TAsk No 3-----------------------------------------------------------------------------
+
+// // 4. Write a script to display following messages in sequence:
+//  alert("Welcome To JS Land");
+//  alert("Happy Coding!");
+
+// // -------------------------------------------------------------TAsk No 4----------------------------------------------------------------------------
+
+// // Generate the following message through browser’s
+// // developer console
+//  alert("hello i can use Js through my web browser console");
